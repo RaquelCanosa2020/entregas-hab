fcdeb458c019fbc0b39692c86eff11eb0bf38006
hab0 <denebolaleo1@gmail.com>
2020-03-17 19:58:00 +0100

    entrega JSII segundo ejercicio

--- js/entrega-2/www/binario.js
+++ js/entrega-2/www/binario.js
@@ -1,1 +1,15 @@
 "use strict";
+
+let binaryNumber = prompt("introduce un número binario");
+
+function binaryConverter(binaryNumber) {
+  let Array = binaryNumber.split("");
+  let suma = 0;
+  for (let i = 0; i < Array.length; i++) {
+    suma = suma + Array[i] * 2 ** (Array.length - (i + 1));
+  }
+  console.log(
+    `el número binario ${binaryNumber} se corresponde con el valor decimal de: ${suma}`
+  );
+}
+binaryConverter(binaryNumber);
--- js/entrega-2/www/letras.js
+++ js/entrega-2/www/letras.js
@@ -1,20 +1,26 @@
 "use strict";
 
-let frase = "hoy hace un día estupendo y fantástico";
+let frase = prompt("introduce una frase");
 
 function devolverPalabra(frase) {
-  let fraseArray = frase.split(" "); //incluyo la frase en un array, cada elemento es una palabra
+  let fraseArray = frase.split(" ");
   let resultado = fraseArray[0].length;
   let posicion;
-  for (let i = 0; i < fraseArray.length; i++) { 
-    if (resultado >= fraseArray[i].length) {  //>= para que si hay dos palabras de = nº letras escoja la primera.
+  for (let i = 0; i < fraseArray.length; i++) {
+    if (resultado >= fraseArray[i].length) {
       resultado;
     } else {
       resultado = fraseArray[i].length;
       posicion = i;
     }
   }
-  return resultado, posicion, console.log(fraseArray[posicion]); // me da la longitud mayor, la posición de ese elemento y me lo imprime
+  return (
+    resultado,
+    posicion,
+    console.log(
+      `la primera palabra más larga de tu frase es "${fraseArray[posicion]}", con ${resultado} letras`
+    )
+  );
 }
 
 devolverPalabra(frase);
