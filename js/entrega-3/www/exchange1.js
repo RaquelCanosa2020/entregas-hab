6cb91f53df13313575dadad14325a720eb76ed23
hab0 <denebolaleo1@gmail.com>
2020-05-16 19:18:49 +0200

    Entrega HTML

--- js/entrega-3/www/exchange1.js
+++ js/entrega-3/www/exchange1.js
@@ -9,7 +9,7 @@ Por último convierte el resultado obtenido en dólares a yenes (JPY).*/
 
 const currency = 25;
 
-async function getInfo(number) {
+/*async function getInfo(number) {
   const requestInfo = await fetch(
     "https://api.exchangerate-api.com/v4/latest/EUR"
   );
@@ -28,4 +28,40 @@ async function getInfo(number) {
   );
 }
 
-getInfo(currency);
+getInfo(currency);*/
+
+//primera parte con then (ejercicio aparte)
+/*console.log(`cantidad inicial:  ${currency} euros`);
+
+function getDollar(quantity) {
+  fetch("https://api.exchangerate-api.com/v4/latest/EUR")
+    .then((response) => response.json())
+    .then((data) => {
+      let dollars = (quantity * data.rates.USD).toFixed(2);
+      console.log(`son ${dollars} USD`);
+    });
+}
+
+getDollar(currency);*/
+
+// para todo el ejercicio, lo hace con async awauit pero utiliza
+// dos veces la api, una /EUR, y otra con /USD. Mucho más fácil:
+
+async function exchange(quantity) {
+  const requestDollars = await fetch(
+    "https://api.exchangerate-api.com/v4/latest/EUR"
+  );
+
+  const infoDollars = await requestDollars.json();
+  const USD = infoDollars.rates.USD * quantity;
+
+  const requestYenes = await fetch(
+    "https://api.exchangerate-api.com/v4/latest/USD"
+  );
+  const infoYenes = await requestYenes.json();
+  const yen = infoYenes.rates.JPY * USD;
+
+  console.log(`${quantity} euros son ${USD} dólares y ${yen} yenes`);
+}
+
+exchange(currency);
